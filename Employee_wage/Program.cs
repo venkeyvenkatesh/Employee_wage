d24aa46db24b8d580f6f6e584c9355ca3aeec09e
venkey <venkeyvenkatesh885@gmail.com>
2020-09-25 10:36:56 +0530

    first commit

---
+++ Employee_wage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
