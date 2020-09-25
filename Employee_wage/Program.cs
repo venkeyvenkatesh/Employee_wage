8d01989a2cdbc4c21ac3b96892926d12a23f6b43
venkeyvenkatesh <42239849+venkeyvenkatesh@users.noreply.github.com>
2020-09-25 15:01:39 +0530

    [venkey] Refactor. Added some extra code

--- Employee_wage/Program.cs
+++ Employee_wage/Program.cs
@@ -8,6 +8,18 @@ namespace Employee_wage
         {
           
            Console.WriteLine("Hello welcome to Employee wage problem");
+            
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
