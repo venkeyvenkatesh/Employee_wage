9dc3a136f6e4235e33fb5fe46e641b0cf157d534
venkeyvenkatesh <42239849+venkeyvenkatesh@users.noreply.github.com>
2020-09-25 10:58:40 +0530

    [venkey] change in the main method

--- Employee_wage/Program.cs
+++ Employee_wage/Program.cs
@@ -7,17 +7,7 @@ namespace Employee_wage
         static void Main(string[] args)
         {
           
-            int IS_FULL_TIME = 1;
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck==IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is Present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+           Console.WriteLine("Hello welcome to Employee wage problem");
         }
     }
 }
